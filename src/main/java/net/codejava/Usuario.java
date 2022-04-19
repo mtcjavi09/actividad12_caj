d91215a7190874e5e50a94f7d67de9af1ceb0f12
Mash <mashjov13@outlook.com>
2022-04-19 10:34:27 -0500

    Se eliminan espacios en AppMain y en Usuario

--- src/main/java/net/codejava/Usuario.java
+++ src/main/java/net/codejava/Usuario.java
@@ -9,25 +9,14 @@ package net.codejava;
  *
  * @author jose.jimenez07
  */
-public class Usuario {
+public class Usuario 
+{
 
     private String idusuario;
     private String password;
 
-    public String getIdusuario() {
-        return idusuario;
-    }
-
-    public void setIdusuario(String idusuario) {
-        this.idusuario = idusuario;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
+    public String getIdusuario() {return idusuario;}
+    public void setIdusuario(String idusuario) {this.idusuario = idusuario;}
+    public String getPassword() {return password;}
+    public void setPassword(String password) {this.password = password;}
 }
